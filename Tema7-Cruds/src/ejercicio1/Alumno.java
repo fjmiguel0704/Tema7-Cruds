c6baad512b530ac7ac2e970aba75ee0b7b408a8d
fjmiguel0704 <fernandoj.miguel@iesnervion.es>
2023-04-25 13:11:19 +0200

    Terminar programa

---
+++ Tema7-Cruds/src/ejercicio1/Alumno.java
@@ -0,0 +1,77 @@
+package ejercicio1;
+
+public class Alumno {
+	/**
+	 * Guardará el nombre del alumno
+	 */
+
+	private String nombre = null;
+	/**
+	 * Guardará la nota media del alumno
+	 */
+	private double notaMedia;
+
+	/**
+	 * Constructor por defecto
+	 */
+	public Alumno() {
+
+	}
+
+	/**
+	 * Constructor que asigna valores a nombre y notaMedia del alumno
+	 * 
+	 * @param nombre    del alumno
+	 * @param notaMedia del alumno
+	 */
+	public Alumno(String nombre, double notaMedia) {
+		this.nombre = nombre;
+		this.notaMedia = notaMedia;
+	}
+
+	/**
+	 * Devuelve el nombre del alumno
+	 * 
+	 * @return nombre
+	 */
+	public String getNombre() {
+		return nombre;
+	}
+
+	/**
+	 * Modifica el nombre del alumno
+	 * 
+	 * @param nombre del alumno
+	 */
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	/**
+	 * Devuelve la nota media del alumno
+	 * 
+	 * @return notaMedia
+	 */
+	public double getNotaMedia() {
+		return notaMedia;
+	}
+
+	/**
+	 * Modifica la nota media del alumno
+	 * 
+	 * @param notaMedia 
+	 */
+	public void setNotaMedia(double notaMedia) {
+		if (notaMedia > 0) {
+			this.notaMedia = notaMedia;
+		}
+	}
+
+	/**
+	 * Muestra el nombre y nota media del alumno
+	 */
+	@Override
+	public String toString() {
+		return "Alumno: " + nombre + "\nNota media: " + notaMedia;
+	}
+}
---
+++ Tema7-Cruds/src/ejercicio1/Main.java
@@ -0,0 +1,138 @@
+package ejercicio1;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		// Creamos el Scanner
+		Scanner read = new Scanner(System.in);
+
+		// Guardará la opcion del usuario
+		int opc = 0;
+
+		// Guardará el nombre del alumno
+		String nombre = " ";
+
+		// Guardará la nota media del alumno
+		double notaMedia = 0;
+
+		// Creamos el array de objetos tipo Alumno
+		Alumno[] alumnos = new Alumno[30];
+
+		// Recorremos el array y lo inicializamos a los valores de alumno, inicialmente
+		// nombre null y nota media 0
+		for (int i = 0; i < alumnos.length; i++) {
+			alumnos[i] = new Alumno();
+		}
+
+		do {
+			// Mostramos el menú de opciones al usuario
+			System.out.println("ALUMNOS");
+			System.out.println("------------------------");
+			System.out.println("1. Listado");
+			System.out.println("2. Nuevo Alumno");
+			System.out.println("3. Modificar");
+			System.out.println("4. Borrar");
+			System.out.println("5. Salir");
+
+			// Leemos la opcion introducida por el usuario
+			opc = read.nextInt();
+
+			switch (opc) {
+
+			// En caso de 1, listará los alumnos
+			case 1:
+
+				// Recorremos el array
+				for (int i = 0; i < alumnos.length; i++) {
+					// Comprobamos que el nombre del alumno sea distinto a null para imprimir el
+					// alumno
+					if (alumnos[i].getNombre() != null) {
+						System.out.println(alumnos[i].toString());
+					}
+				}
+				break;
+
+			// En caso de 2, introduciremos un nuevo alumno
+			case 2:
+				// Pedimos el nombre del nuevo alumno
+				System.out.println("Introduce el nombre del alumno");
+				nombre = read.next();
+
+				// Pedimos la nota media del alumno
+				System.out.println("Introduce la nota media del alumno");
+				notaMedia = read.nextDouble();
+
+				// Mientras la nota media sea menor a cero, volvemos a pedirle la nota
+				while (notaMedia < 0) {
+					System.out.println("Introduce de nuevo la nota media del alumno");
+					notaMedia = read.nextDouble();
+				}
+
+				// Recorremos el array
+				for (int i = 0; i < alumnos.length; i++) {
+					// Y comprobamos en que posición del array no se encuentra un alumno
+					if (alumnos[i].getNombre() == null) {
+						// Introducimos el nombre y nota media del alumno
+						alumnos[i].setNombre(nombre);
+						alumnos[i].setNotaMedia(notaMedia);
+					}
+				}
+				break;
+
+			// En caso de 3, modificamos a un alumno
+			case 3:
+				// Pedimos el nombre del alumno a modificar
+				System.out.println("Introduce el nombre del alumno a modificar");
+				nombre = read.next();
+
+				// Recorremos el array
+				for (int i = 0; i < alumnos.length; i++) {
+					// Comprobamos en que posición del array se encuentra ese nombre
+					if (alumnos[i].getNombre().equals(nombre)) {
+						// Y le pedimos la nueva nota del alumno
+						System.out.println("Introduce la nueva nota del alumno:");
+						notaMedia = read.nextDouble();
+						// Mientras que la nota sea menor a 0, se la volvemos a pedir
+						while (notaMedia < 0) {
+							System.out.println("Introduce de nuevo la nota media del alumno");
+							notaMedia = read.nextDouble();
+						}
+					}
+				}
+				break;
+
+			// En caso de 4, eliminaremos un alumno
+			case 4:
+				// Pedimos el nombre del alumno a eliminar
+				System.out.println("Introduce el nombre del alumno a eliminar:");
+				nombre = read.next();
+
+				// Recorremos array
+				for (int i = 0; i < alumnos.length; i++) {
+					// Y comprobamos en que posición se encuentra el alumno con ese nombre
+					if (alumnos[i].getNombre().equals(nombre)) {
+						// Actualizamos el nombre del alumno a null
+						alumnos[i].setNombre(null);
+					}
+				}
+				break;
+			// En caso de que la opción introducida sea otra, mostrará un mensaje de no
+			// válida
+			default:
+				System.out.println("Opción incorrecta");
+			}
+
+		} while (opc != 5); // Mientras que la opcion sea distinta de 5
+
+		// Mostramos un mensaje de que se cerró el programa
+		System.out.println("Cerrando programa...");
+
+		// Cerramos el Scanner
+		read.close();
+
+	}
+
+}
